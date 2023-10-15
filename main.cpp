2f7a7198389d783c415ce175c45535c134be994e
vyahislav <khv-rvv@mail.ru>
2023-10-15 20:17:25 +1000

    first commit

---
+++ main.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+#include<vector>
+#include<set>
+#include<list>
+#include<string>
+
+template<class T>
+void print_container(const T& container)
+{
+	for (auto it = container.begin(); it != container.end(); ++it)
+		std::cout << *it << ", ";
+	std::cout << "\n";
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	std::cout << "\nПечать контейнера sdt::set: ";
+	std::set<std::string> test_set = { "one","two","three","four" };
+	print_container(test_set);
+
+	std::cout << "\nПечать контейнера sdt::list: ";
+	std::list<std::string> test_list = { "one","two","three","four" };
+	print_container(test_list);
+
+	std::cout << "\nПечать контейнера sdt::vector: ";
+	std::vector<std::string> test_vector = { "one","two","three","four" };
+	print_container(test_vector);
+
+	return 0;
+}
